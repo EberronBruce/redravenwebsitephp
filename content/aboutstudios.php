7eda2ccb88e30babcc9fdc67291172cdf39a4e9c
Bruce Burgess <bruce@redravencomputing.com>
2015-08-18 21:35:31 +0700

    Worked on the about studio page, added my profile and some buttons.

--- content/aboutstudios.php
+++ content/aboutstudios.php
@@ -1,10 +1,65 @@
 <section>
+    <div id="studio">
+    <article class="studio">
+        <h1>About the Studios</h1>
+        <p>We are a small group of engineers who are passionate about technology. 
+        We started this small studio to be able to follow our passion in developing
+        ideas and making new and improve solutions to problems without dealing with
+        the bureaucracy that plagues most organizations. This leaves us more time to
+        pursue and focus our efforts into the projects we take.</p>
+        <p>We have decided to open up some of our services to the world. We offer all
+        kinds of technical services from consulting, user experience design, 
+        software migration to building websites, mobile applications such as iOS and
+        more. We are also interested in ubiquitous computing projects. If you 
+        any technical services or just need a website or app, please 
+        contact us. With our background in engineering and technology, we can probably help.</p>
+    </article>
     
-    <h1>Studios page</h1>
-    <figure style = "text-align: center">
-        <img src="image/construct.png" alt="Page Under Construction" /> 
+     
+    <br />
+    <br />
+    <hr>
+    
+    
+    <h1>Meet Our Team</h1>
+    <figure class="pic">
+        <img src="image/bruce.jpg" alt="Picture of Bruce" class="portrait" />
+        <button class="profilebtn">
+             <img class="leftraven" src="image/navleftraven.png" alt="Left Red Raven" />
+             View My Profile
+             <img  class="rightraven" src="image/navrightraven.png" alt="Right Red Raven" />
+        </button>       
+        <button class="profilebtn">
+            <img class="leftraven" src="image/navleftraven.png" alt="Left Red Raven" />
+            &nbsp;View My Blog&nbsp;
+            <img  class="rightraven" src="image/navrightraven.png" alt="Right Red Raven" />
+        </button>
+        <br />
     </figure>
-    <h1>We are sorry but this page is under construction.</h1>
+    <article class="team">
+        <p>Hello, my name is Bruce Burgess. I am the founder of Red Raven 
+        Computing Studios. I have always been fascinated by computers ever since
+        I was a child. I started my computer journey by using DOS and by playing a game
+        called Zork. I also like Nintendo growing up. I started to familiarize 
+        myself with widows and helped my local elementary school's librarian with computers when I was 
+        in elementary. This curiosity and interest is still with me today.</p>
+        <p>I have earned a BS in Electrical Engineering with honors from University
+        of Nevada, Reno and earned a Masters of Engineering from Chung Ang University
+        from Seoul, Korea. I am a bit of a globetrotter as I have traveled and lived
+        in several different countries in Asia. I have worked in Japan as an iOS
+        Developer and System Consultant and I have also did some iOS development
+        in Vietnam.</p>
+        <p>My current interests now are using HTML5, CSS3, Javascript, JQuery, 
+        and other web technologies in building websites and mobile applications
+        as well as iOS development using Xcode, Swift, and Objective C and
+        Android development using Android Studios. I also interested in 
+        using Java in some application. I have some experience with C++. I have 
+        studied about user experience design as well. I am particular interested
+        in getting into ubiquitous computing development which my education can
+        really help in.</p>
+        
+        
+    </article>
     
-    
+    </div>
 </section>
