599dddad4be788dced3a1e4f2a8468e96cdb1797
Bruce Burgess <bruce@redravencomputing.com>
2015-08-30 15:59:29 +0700

    Worked on the home page and adding space fore a video

--- content/home.php
+++ content/home.php
@@ -1,10 +1,36 @@
 <section>
-    
-    <h1>Home page</h1>
-    <figure style = "text-align: center">
-        <img src="image/construct.png" alt="Page Under Construction" /> 
+    <div class="center">
+        <h1>Welcome to Red Raven Computing Studios</h1>
+        <video id="introvid" controls height="450" preload="auto">
+        <source type="video/ogg; codecs=theora,vorbis" src="media/test.ogv" />
+        <source src="media/test.webm" />
+        <source src="media//test.mp4" />
+             Your browser doesn't support video, you may download the
+             video instead: <a href="media/test.mp4">test</a>
+        </video>
+    </div>
+    <figure class="raven">
+        <img id="bigraven" src="image/raven.png" alt="A Big Red Raven" /> 
     </figure>
-    <h1>We are sorry but this page is under construction.</h1>
-    
-    
+
+    <article id="homeArticle">
+        <h1>What We Can Do For You</h1>
+        <p>
+            Thank you for visiting us. We are glad you are here. We view 
+            computing and coding as an art form. We design and build 
+            websites, write mobile applications, and other computing 
+            solutions. We are also interested in ubiquitous development 
+            as well. 
+        </p>
+        <p>
+            Whether you have an idea for a mobile app, or whether you just 
+            want a website for your small business, even if you have a limited 
+            budget or you have no money at all. Or you don't even know how to 
+            how to even start a project in computing, do not let that hold you
+            back from back from achieving you goals. Contact us and let us use 
+            our technical knowledge and skills to help make your dreams a reality. 
+        </p>
+        
+    </article>
+
 </section>
